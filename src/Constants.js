b4b3a4d07d2f6120e381f47528742c7c1439c33c
Ryan <ryantrainor24@gmail.com>
2021-02-02 19:12:09 +0000

    update host

--- src/Constants.js
+++ src/Constants.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:8080' 
+//export const API_URL = 'http://localhost:8080' 
+//export const API_URL = 'http://spring/8080' 
+export const API_URL = 'http://ec2-3-10-212-99.eu-west-2.compute.amazonaws.com:8080' 
